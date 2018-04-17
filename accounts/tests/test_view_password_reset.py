d4a75981446ce12f8318469cd316de8f3fbcc2df
William Yang <505741310@qq.com>
2018-04-17 19:47:18 +0800

    Initial commit

---
+++ accounts/tests/test_view_password_reset.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+
+from django.test import TestCase
+
+from django.urls import reverse, resolve
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import forms
+from django.contrib.auth.models import User
+from django.core import mail
+
+
+class PasswordResetTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset')
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/reset/')
+        self.assertEquals(view.func.view_class, auth_views.PasswordResetView)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, forms.PasswordResetForm)
+
+    def test_form_inputs(self):
+        self.assertContains(self.response, '<input', 2)
+        self.assertContains(self.response, 'type="email"', 1)
+
+
+class SuccessfulPasswordResetTests(TestCase):
+    def setUp(self):
+        email = 'John@example.com'
+        User.objects.create_user(
+            username='John',
+            email=email,
+            password='Qq123456'
+        )
+        url = reverse('password_reset')
+        self.response = self.client.post(url, {'email': email})
+
+    def test_redirection(self):
+        url = reverse('password_reset_done')
+        self.assertRedirects(self.response, url)
+
+    def test_send_password_reset_email(self):
+        self.assertEqual(1, len(mail.outbox))
+
