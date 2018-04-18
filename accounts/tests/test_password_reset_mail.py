ba0fc7e68ec9b130efba1432b0f8d61d00fd5363
William Yang <505741310@qq.com>
2018-04-18 18:16:34 +0800

    增加password_reset部分网页和测试

---
+++ accounts/tests/test_password_reset_mail.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+from django.core import mail
+from django.contrib.auth.models import User
+from django.urls import reverse, resolve
+from django.test import TestCase
+
+
+class PasswordResetMailTests(TestCase):
+    def setUp(self):
+        email = 'John@example.com'
+        User.objects.create_user(
+            username='John',
+            email=email,
+            password='Qq123456'
+        )
+        url = reverse('password_reset')
+        self.response = self.client.post(url, {'email': email})
+        self.email = mail.outbox[0]
+
+    def test_email_subject(self):
+        self.assertEquals('[Django Boards] Please reset your password', self.email.subject)
+
+    def test_email_body(self):
+        context = self.response.context
+        token = context.get('token')
+        uid = context.get('uid')
+        password_reset_token_url = reverse(
+            'password_reset_confirm',
+            kwargs={'uidb64': uid, 'token': token}
+        )
+        self.assertIn(password_reset_token_url, self.email.body)
+        self.assertIn('John', self.email.body)
+        self.assertIn('John@example.com', self.email.body)
+
+    def test_email_to(self):
+        self.assertEquals(['John@example.com'], self.email.to)
