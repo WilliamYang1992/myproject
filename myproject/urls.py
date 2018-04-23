26082ed9286bee926777eb6b511a15b0eadde5e9
William Yang <505741310@qq.com>
2018-04-23 18:26:58 +0800

    增加my_account界面

--- myproject/urls.py
+++ myproject/urls.py
@@ -53,5 +53,6 @@ urlpatterns = [
     path('settings/password/done', auth_views.PasswordChangeDoneView.as_view(
         template_name='password_change_done.html'
     ), name='password_change_done'),
+    path('settings/account/', account_views.UserUpdateView.as_view(), name='my_account'),
     path('admin/', admin.site.urls),
 ]
